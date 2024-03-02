fa611c7092d3e7db4d7ea62b5fc27e68a32d88a8
schoolUses <155615963+schoolUses@users.noreply.github.com>
2024-03-02 21:41:27 +0700

    Add files via upload
    tf happened?

---
+++ Parser_.py
@@ -0,0 +1,31 @@
+from Variables import *
+from Error import*
+
+class Parser:
+  def __init__(self, tokens):
+    self.tokens = tokens
+    self.tok_idx = -1
+    self.advance()
+
+  def advance(self):
+    self.tok_idx += 1
+    self.update_current_tok()
+    return self.current_tok
+
+  def reverse(self, amount=1):
+    self.tok_idx -= amount
+    self.update_current_tok()
+    return self.current_tok
+
+  def update_current_tok(self):
+    if self.tok_idx >= 0 and self.tok_idx < len(self.tokens):
+      self.current_tok = self.tokens[self.tok_idx]
+
+  def parse(self):
+    res = self.statements()
+    if not res.error and self.current_tok.type != TT_EOF:
+      return res.failure(InvalidSyntaxError(
+        self.current_tok.pos_start, self.current_tok.pos_end,
+        "Token cannot appear after previous tokens"
+      ))
+    return res
